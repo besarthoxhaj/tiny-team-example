1a3fdf53d5252b327bd42d956033cb3f40fda667
besarthoxhaj <besartshyti@gmail.com>
2023-07-20 15:45:05 +0200

    [config] - default redis ports

--- app/main.py
+++ app/main.py
@@ -11,7 +11,7 @@ app = fastapi.FastAPI()
 
 @app.on_event("startup")
 async def startup_event():
-  app.state.r = redis.Redis(host="redis", port=6378, db=0, password="MvY4bQ7uN3")
+  app.state.r = redis.Redis(host="redis", port=6379, db=0, password="MvY4bQ7uN3")
   app.state.k = confluent_kafka.Producer({"bootstrap.servers": "kafka:29092"})
 
 
